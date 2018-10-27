94290d0770312b76c9d9a6be58dc356afcfc5fb3
Thomas <thomas@craftacademy.se>
2018-10-27 09:52:16 +0200

    refactors strategy with right response and urls to CraftEd servers

--- lib/crafted_oauth.rb
+++ lib/crafted_oauth.rb
@@ -1,44 +1,25 @@
+# frozen_string_literal: true
+
 require 'omniauth/strategies/oauth2'
 require 'date'
 
 module OmniAuth
   module Strategies
     class CraftedOauth < OmniAuth::Strategies::OAuth2
-      # Give your strategy a name.
-      option :name, 'crafted'
-      
-      FORCE_APPROVAL_KEY = 'ommiauth_crafted_force_approval?'.freeze
+      option :name, 'crafted_oauth'
 
-      # This is where you pass the options you would pass when
-      # initializing your consumer from the OAuth gem.
-      option :client_options, {
-        site:           'https://class.craftacademy.co',
-        authorize_url:  'https://class.craftacademy.co/oauth2/authorize',
-        token_url:      'https://class.craftacademy.co/oauth2/token',
-      }
+      option :client_options,
+             site:           'https://class.craftacademy.co',
+             authorize_url:  'https://class.craftacademy.co/oauth2/authorize',
+             token_url:      'https://class.craftacademy.co/oauth2/access_token'
 
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
-      uid{ raw_info['id'] }
+      uid { raw_info['id'] ||= SecureRandom.uuid }
 
       info do
         {
-          # Unless the 'user-read-private' scope is included, the birthdate, country, image, and product fields may be nil,
-          # and the name field will be set to the username/nickname instead of the display name.
-          # The email field will be nil if the 'user-read-email' scope isn't included.
-          #
-          :name => raw_info['display_name'] || raw_info['id'],
-          :nickname => raw_info['id'],
-          :email => raw_info['email'],
-          :urls => raw_info['external_urls'],
-          :image => image_url,
-          :birthdate => raw_info['birthdate'] && Date.parse(raw_info['birthdate']),
-          :country_code => raw_info['country'],
-          :product => raw_info['product'],
-          :follower_count => raw_info['followers']['total']
+          email: raw_info['email'],
+          first_name: raw_info['name'],
+          image_url: image_url
         }
       end
 
@@ -49,33 +30,11 @@ module OmniAuth
       end
 
       def image_url
-        if images = raw_info['images']
-          if first = images.first
-            first['url']
-          end
-        end
+        raw_info['profile_image']['image_url_full'] if raw_info['profile_image']['has_image']
       end
 
       def raw_info
-        @raw_info ||= access_token.get('me').parsed
-      end
-      
-      def authorize_params
-        super.tap do |params|
-          if session.delete(FORCE_APPROVAL_KEY) ||
-             (session[:flash] && session[:flash]['flashes'] && session[:flash]['flashes'][FORCE_APPROVAL_KEY])
-            params[:show_dialog] = true
-          end
-        end
-      end
-
-      def request_phase
-        %w[show_dialog].each do |v|
-          if request.params[v]
-            options[:authorize_params][v.to_sym] = request.params[v]
-          end
-        end
-        super
+        @raw_info ||= access_token.get('/api/user/v1/accounts').parsed.first
       end
 
       def callback_url
